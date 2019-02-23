6237c970e664d5e35673c34dd800ee6fc93f91eb
3LOGIXS\sahan_b <sahan.b@eyepax.com>
2019-02-23 17:21:12 +0530

    react-9

--- React/src/index.js
+++ React/src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Header from "./Components/Header/Header";
 import Loader from "./Components/Loader/Loader";
+
+import RantList from "./Scenes/RantList";
+import RantDetails from "./Scenes/RantDetails";
 
 import "./css/app.css";
 
@@ -17,7 +20,12 @@ function App() {
                 <Header/>
 
                 {/*Main layout*/}
-                <section className="main layout--center"></section>
+                <section className="main layout--center">
+                    <div className="main__content layout--wrapped">
+                        <Route exact path="/" component={RantList} />
+                        <Route exact path="/rant/1" component={RantDetails} />
+                    </div>
+                </section>
 
                 <Loader isLoading={false}/>
             </div>
